13dd9c85310093d42bed1528c07aec397cb76716
nils <nils@risc0.com>
2022-07-26 19:05:52 +0000

    Add rust standard library support for risc0 zkvm.

--- library/std/src/sys/zkvm/stdio.rs
+++ library/std/src/sys/zkvm/stdio.rs
@@ -58,6 +58,6 @@ pub fn is_ebadf(_err: &io::Error) -> bool {
     true
 }
 
-pub fn panic_output() -> Option<Vec<u8>> {
+pub fn panic_output() -> Option<impl io::Write> {
     Some(Stderr::new())
 }
